623b95637f86897ad563503b1498000cd4c6aaaf
Tranix <tranix@email.com>
2026-03-16 22:57:18 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  const greeting = 'Hello world!'
+  return (
+    <div className="App">
+      <h1 className="sample">
+        waw
+       {greeting}
+      </h1>
+    </div>
+    
+  )
+}
+
+export default App
