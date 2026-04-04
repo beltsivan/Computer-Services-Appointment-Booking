e4651d03bac77a4688242b115b121b139365697e
Tranix <tranix@email.com>
2026-04-05 03:14:39 +0800

    Initial Pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Header } from "../components/Header"
+import { Hero } from "../components/hero"
+import { Service } from "../components/service"
+import { Footer } from "../components/footer"
+
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Service />
+      <Footer />
+    </>
+  )
+}
